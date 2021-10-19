5958b950de4771fbd6f7b4f2407355071be7c841
Matteo Busi <matteo.busi@prima.it>
2021-10-19 17:48:46 +0200

    Bump opentelemetry deps to 1.0.0.rc-3 (#21)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OpentelemetryAbsinthe.MixProject do
   def project do
     [
       app: :opentelemetry_absinthe,
-      version: "1.0.0-rc.5",
+      version: "1.0.0-rc.6",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -24,7 +24,7 @@ defmodule OpentelemetryAbsinthe.MixProject do
     [
       {:absinthe, ">= 1.5.0", optional: true},
       {:jason, "~> 1.2"},
-      {:opentelemetry_api, "~> 1.0.0-rc.2"},
+      {:opentelemetry_api, "~> 1.0.0-rc.3"},
       {:telemetry, "~> 0.4 or ~> 1.0.0"}
     ] ++ dev_deps()
   end
@@ -32,7 +32,8 @@ defmodule OpentelemetryAbsinthe.MixProject do
   defp dev_deps do
     [
       {:absinthe_plug, "~> 1.5", only: :test},
-      {:opentelemetry, "~> 1.0.0-rc.2", only: :test},
+      {:opentelemetry, "~> 1.0.0-rc.3", only: :test},
+      {:opentelemetry_exporter, "~> 1.0.0-rc.3", only: :test},
       {:plug_cowboy, "~> 2.2", only: :test},
       {:credo, "~> 1.4", only: [:dev, :test]},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
