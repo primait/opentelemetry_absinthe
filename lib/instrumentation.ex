74e42444718874e6f0af4ac6078a4b05375193d0
Cristiano Piemontese <cristiano.piemontese@prima.it>
2023-02-24 10:00:07 +0100

    make selections lazy

--- lib/instrumentation.ex
+++ lib/instrumentation.ex
@@ -73,18 +73,6 @@ defmodule OpentelemetryAbsinthe.Instrumentation do
   end
 
   def handle_operation_stop(_event_name, _measurements, data, config) do
-    fields =
-      case data do
-        %{blueprint: %{operations: [_ | _] = operations}} ->
-          operations
-          |> Enum.flat_map(& &1.selections)
-          |> Enum.map(& &1.name)
-          |> Enum.uniq()
-
-        _ ->
-          []
-      end
-
     errors = data.blueprint.result[:errors]
 
     result_attributes =
@@ -97,9 +85,9 @@ defmodule OpentelemetryAbsinthe.Instrumentation do
         config.trace_response_errors,
         {"graphql.response.errors", Jason.encode!(errors)}
       )
-      |> put_if(
+      |> lazy_put_if(
         config.trace_request_selections,
-        {"graphql.request.selections", Jason.encode!(fields)}
+        fn -> {"graphql.request.selections", data |> get_graphql_selections() |> Jason.encode!()} end
       )
 
     set_status(errors)
@@ -111,12 +99,28 @@ defmodule OpentelemetryAbsinthe.Instrumentation do
     :ok
   end
 
+  defp get_graphql_selections(data) do
+    case data do
+      %{blueprint: %{operations: [_ | _] = operations}} ->
+        operations
+        |> Enum.flat_map(& &1.selections)
+        |> Enum.map(& &1.name)
+        |> Enum.uniq()
+
+      _ ->
+        []
+    end
+  end
+
   # Surprisingly, that doesn't seem to by anything in the stdlib to conditionally
   # put stuff in a list / keyword list.
   # This snippet is approved by José himself:
   # https://elixirforum.com/t/creating-list-adding-elements-on-specific-conditions/6295/4?u=learts
   defp put_if(list, false, _), do: list
   defp put_if(list, true, value), do: [value | list]
+
+  defp lazy_put_if(list, false, _), do: list
+  defp lazy_put_if(list, true, value_fn), do: [value_fn.() | list]
 
   # taken from https://github.com/opentelemetry-beam/opentelemetry_plug/blob/82206fb09fbeb9ffa2f167a5f58ea943c117c003/lib/opentelemetry_plug.ex#L186
   @ctx_key {__MODULE__, :parent_ctx}
