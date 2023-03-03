c54cb5b1691b546413a6bc9c1aad946c865860ca
mae.kasza <26093674+MaeIsBad@users.noreply.github.com>
2023-03-03 14:16:52 +0100

    Fix test support code being compiled in dev
    Since we don't install test dependencies in dev environments the code
    can't compile/execute anyway

--- mix.exs
+++ mix.exs
@@ -16,7 +16,6 @@ defmodule OpentelemetryAbsinthe.MixProject do
   end
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(:dev), do: ["lib", "test/support"]
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
