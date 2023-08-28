90473ca25209e9a0e425a421cc558b7a30696f1b
Cristiano Piemontese <cristiano.piemontese@prima.it>
2023-08-28 15:47:11 +0200

    bump min elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule OpentelemetryAbsinthe.MixProject do
     [
       app: :opentelemetry_absinthe,
       version: "2.0.1",
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
